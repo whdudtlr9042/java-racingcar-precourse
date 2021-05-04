3dd9082888cd6fbee81cd71963efc2a9ebdcf31e
whdudtlr9042 <ys9042@gmail.com>
2021-05-04 22:36:40 +0900

    자동차 경주 feat 컨트롤러 구현, 이름 및 경기 횟수 입력

---
+++ src/main/java/controller/RacingCarController.java
@@ -0,0 +1,19 @@
+package controller;
+
+import view.InputView;
+import view.ResultView;
+
+public class RacingCarController {
+
+    private final InputView inputView;
+    private final ResultView resultView;
+
+    public RacingCarController() {
+        this.inputView = new InputView();
+        this.resultView = new ResultView();
+    }
+
+
+    public void run() {
+    }
+}
---
+++ src/main/java/main/RacingCarMain.java
@@ -0,0 +1,9 @@
+package main;
+
+import controller.RacingCarController;
+
+public class RacingCarMain {
+    public static void main(String[] args) {
+        new RacingCarController().run();
+    }
+}
